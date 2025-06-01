9e0d69ecdb3ce1de34a69733c32b738759e1961e
Juvens Helas <407juvens@gmail.com>
2025-06-01 11:37:46 -0700

    Update Announcement.js

--- backend/models/Announcement.js
+++ backend/models/Announcement.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const announcementSchema = new mongoose.Schema({
+  title: String, // ✅ New field
   message: String,
   createdAt: { type: Date, default: Date.now }
 });
